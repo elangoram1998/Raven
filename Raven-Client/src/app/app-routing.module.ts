c8192555138da1deda7aae12679d9dcf46a958f7
elangoram1998@gmail.com <elangoram1998@gmail.com>
2021-02-11 21:31:24 +0530

    sign in and sign up page

--- Raven-Client/src/app/app-routing.module.ts
+++ Raven-Client/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { MainLayoutComponent } from './main-layout/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'SignIn', pathMatch: 'full' },
+  { path: 'SignIn', component: SignInComponent },
+  { path: 'SignUp', component: SignUpComponent },
+  {
+    path: 'home', component: MainLayoutComponent,
+    children: []
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Raven-Client/src/app/app.module.ts
+++ Raven-Client/src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material/material.module';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { MainLayoutModule } from './main-layout/main-layout.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    MainLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
